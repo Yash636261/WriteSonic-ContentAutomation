43f96f982d701c335337d763f2f54002702c9848
Nishitbaria <Nishitbaria@gmail.com>
2023-05-17 12:52:42 +0530

    React + Tailwind setup is ready

---
+++ WriteSonic/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Setup is complete!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
